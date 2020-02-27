3e365944b767dceeb008e2301efa79e84107f4f9
brandenlacour <brandenlacour@gmail.com>
2020-02-27 11:13:33 -0600

    working on redux

---
+++ src/reducer.js
@@ -0,0 +1,13 @@
+const defaultState = {
+  loggedIn: false,
+  drawerOpen: false
+};
+
+function reducer(state = defaultState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default reducer;
