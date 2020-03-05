23686a3ff64a92d6a621ff1acc98ee429ab795d9
brandenlacour <brandenlacour@gmail.com>
2020-03-05 10:24:10 -0600

    debugging

--- src/FormsContainer/index.js
+++ src/FormsContainer/index.js
@@ -55,13 +55,13 @@ class FormsContainer extends React.Component {
     console.log(companyInfo, "is the company info");
     try {
       const companyResponse = await fetch(
-        "https://bar-savior-api.herokuapp.com/api/v1/companys/",
+        process.env.REACT_APP_API_URL + "/api/v1/companys/",
         {
           credentials: "include",
           method: "POST",
           body: JSON.stringify(companyInfo),
           headers: {
-            "content-type": "application/json"
+            "Content-Type": "application/json"
           }
         }
       );
@@ -86,7 +86,7 @@ class FormsContainer extends React.Component {
           method: "POST",
           body: JSON.stringify(userInfo),
           headers: {
-            "content-type": "application/json"
+            "Content-Type": "application/json"
           }
         }
       );
@@ -120,7 +120,7 @@ class FormsContainer extends React.Component {
           method: "POST",
           body: JSON.stringify(userInfo),
           headers: {
-            "content-type": "application/json"
+            "Content-Type": "application/json"
           }
         }
       );
